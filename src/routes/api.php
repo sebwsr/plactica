6e69c1cee6aeac71f8ac561e741513a967a07339
sebastian <sebwsrz@gmail.com>
2025-06-11 15:24:52 -0600

    archivos correctos

---
+++ src/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+// Agregar el controlador EventoController
+use App\Http\Controllers\EventoController;
+/**
+* Rutas para el recurso Evento.
+*/
+// Recuperar todos los eventos
+Route::get('/eventos', [EventoController::class, 'index']);
+// Almacenar un evento nuevo
+Route::post('/eventos', [EventoController::class, 'store']);
+// Recuperar un evento específico
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+// Actualizar un evento específico
+Route::put('/eventos/{evento}', [EventoController::class, 'update']);
+// Eliminar un evento específico
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
